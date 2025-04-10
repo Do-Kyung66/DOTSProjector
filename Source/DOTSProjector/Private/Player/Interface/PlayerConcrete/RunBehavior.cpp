c5d49e972111bb6fc27c9d8c32ecfc9c7691dbbb
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-04-11 08:44:09 +0900

    fix(palyer) : 달리기 방향 수정 완료

--- Source/DOTSProjector/Private/Player/Interface/PlayerConcrete/RunBehavior.cpp
+++ Source/DOTSProjector/Private/Player/Interface/PlayerConcrete/RunBehavior.cpp
@@ -16,25 +16,17 @@ void URunBehavior::ExecuteBehavior(AActor* Owner, const FInputActionValue& Value
 
 
 	FVector CurrentVelocity = Player->GetVelocity();
-	bool bIsMoving = !CurrentVelocity.IsNearlyZero(1.0f);
-	FVector Forward = Player->GetActorForwardVector();
-	FVector Right = Player->GetActorRightVector();
-
-
-	FVector2D InputVec = Value.Get<FVector2D>();
-	FVector MoveDir = (Forward * InputVec.X) + (Right * InputVec.Y);
 	
-	if (bIsMoving && Player->CurrentStamina > 0)
+	if (CurrentVelocity.Size() > 0 && Player->CurrentStamina > 0)
 	{
 		Player->bIsRunning = true;
-		Player->AddMovementInput(MoveDir.GetSafeNormal(), 2.0f);
-
-		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, FString::Printf(TEXT("MoveDir: %s"), *MoveDir.ToString()));
+		Player->AddMovementInput(CurrentVelocity.GetSafeNormal(), 2.0f);
 
 		// Stamina
 		Player->CurrentStamina -= Player->StaminaDrainRate * GetWorld()->GetDeltaSeconds();
 		Player->CurrentStamina = FMath::Clamp(Player->CurrentStamina, 0.0f, Player->MaxStamina);
 		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("Player Run"));
 	}
-	
+
+		
 }
