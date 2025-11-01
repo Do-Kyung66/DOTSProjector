0c13ce7f6fbe839d2b3334ba075b4870ede278fd
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-11-01 11:28:19 +0900

    feat 퀘스트 제작 - 위젯 생성

---
+++ Source/DOTSProjector/Public/QuestAcceptWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "QuestAcceptWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOTSPROJECTOR_API UQuestAcceptWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
